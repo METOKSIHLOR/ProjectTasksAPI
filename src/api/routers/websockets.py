b0335bcc0d1f6e09d732a4899780fa7a7364785b
METOKS <pressfschool@gmail.com>
2026-04-22 16:56:51 +0200

    added check permissions to websockets subscribe

--- src/api/routers/websockets.py
+++ src/api/routers/websockets.py
@@ -1,12 +1,18 @@
 from typing import List
+from uuid import UUID
 
 from fastapi import APIRouter, Cookie
 from fastapi.websockets import WebSocket, WebSocketDisconnect
 
 from collections import defaultdict
 
+from sqlalchemy.ext.asyncio import AsyncSession
+
 from src.core.exceptions.users_exceptions import UserNotAuthorizedException
 from src.db.redis_storage import storage
+import src.db.session as sess
+from src.db.repositories.project_repo import ProjectRepository
+from src.db.repositories.tasks_repo import TasksRepository
 
 router = APIRouter()
 
@@ -18,6 +24,34 @@ async def get_current_user(session_id):
 
     return user_id
 
+async def check_user_access(room: str, user_id: str, session: AsyncSession, extra_data: dict | None):
+    try:
+        resource_type, resource_id = room.split(":")
+
+        match resource_type:
+            case "user":
+                return UUID(resource_id) == UUID(user_id)
+            case "project":
+                repo = ProjectRepository(session)
+                member = await repo.get_project_member(project_id=UUID(resource_id), member_id=UUID(user_id))
+                return member is not None
+
+            case "task":
+                if not extra_data or extra_data.get("project_id", None) is None:
+                    return False
+                project = extra_data["project_id"]
+                task_repo = TasksRepository(session)
+                project_repo = ProjectRepository(session)
+                if await project_repo.get_project_member(project_id=UUID(project), member_id=UUID(user_id)):
+                    if await task_repo.get_task_by_project(task_id=UUID(resource_id), project_id=UUID(project)):
+                        return True
+                return False
+
+            case _:
+                return False
+    except (TypeError, ValueError):
+        return False
+
 class ConnectionManager:
     def __init__(self):
         self.rooms = defaultdict(list)
@@ -25,7 +59,8 @@ class ConnectionManager:
     async def connect(self, websocket: WebSocket, rooms: List[str]):
         """Подключить websocket к одной или нескольким комнатам."""
         for room in rooms:
-            self.rooms[room].append(websocket)
+            if websocket not in self.rooms[room]:
+                self.rooms[room].append(websocket)
 
     def disconnect(self, websocket: WebSocket, rooms: List[str]):
         """Отключить websocket от указанных комнат."""
@@ -40,8 +75,6 @@ class ConnectionManager:
 
     async def send_to_room(self, room: str, message: dict):
         """Отправить сообщение всем участникам комнаты."""
-        print(f"Sending to {room}. Active rooms: {list(self.rooms.keys())}")
-        print(f"Users in this room: {len(self.rooms[room])}")
         for ws in self.rooms[room]:
             try:
                 await ws.send_json(message)
@@ -61,7 +94,7 @@ async def websocket_endpoint(websocket: WebSocket, session_id: str = Cookie(None
         user_id = await get_current_user(session_id)
     except UserNotAuthorizedException as e:
         await websocket.close()
-        raise e
+        return e
 
     await websocket.accept()
 
@@ -73,15 +106,24 @@ async def websocket_endpoint(websocket: WebSocket, session_id: str = Cookie(None
             message = await websocket.receive_json()
             action = message.get("action")
             rooms = message.get("rooms", [])
+            extra_data = message.get("extra_data", None)
 
             # Обработка различных действий с помощью match
             match action:
                 case "subscribe":
-                    if rooms:
-                        await manager.connect(websocket, rooms)
+                    allowed_rooms = []
+                    async with sess.SessionFactory() as session:
+                        for room in rooms:
+                            if await check_user_access(room=room, user_id=user_id, session=session, extra_data=extra_data):
+                                allowed_rooms.append(room)
+                    answer = {"success": True} if len(allowed_rooms) > 0 else {"success": False}
+                    await manager.connect(websocket, allowed_rooms)
+                    await websocket.send_json(answer)
+
                 case "unsubscribe":
                     if rooms:
                         manager.disconnect(websocket, rooms)
+
                 case _:
                     # Если действие не распознано
                     await websocket.send_json({"error": "Invalid action"})
