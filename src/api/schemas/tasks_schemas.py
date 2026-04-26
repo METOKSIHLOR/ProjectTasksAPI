6a54a0deda552235b474b505a9d6501e0b1dec95
METOKS <pressfschool@gmail.com>
2026-04-26 11:24:54 +0200

    fixed validation bug in task schemas

--- src/api/schemas/tasks_schemas.py
+++ src/api/schemas/tasks_schemas.py
@@ -1,6 +1,6 @@
 from uuid import UUID
 
-from pydantic import BaseModel, EmailStr, Field
+from pydantic import BaseModel, EmailStr, Field, field_validator
 
 from src.db.models import AllowedTaskStatus
 
@@ -43,6 +43,13 @@ class TaskInfoSchemaWithOwnerEmail(TaskInfoSchema):
 
 class UpdateTaskSchema(TaskSchema):
     """Схема для обновления данных в таске. Для полей которые обновлять не надо оставить None"""
+    title: str | None = Field(None, title="Название таски",
+                              description="Если обновлять поле не надо - оставить None",
+                              examples=["Create web API"])
+    description: str | None = Field(None, title="Описание таски",
+                                    description="Если обновлять поле не надо - оставить None",
+                                    examples=["This API should be create tommorow"])
+
     status: AllowedTaskStatus | None = Field(None, title="Статус задачи",
                                                    description='Если обновлять поле не надо - оставить None',
                                                    examples=["todo", "in_progress", "done"])
@@ -50,3 +57,17 @@ class UpdateTaskSchema(TaskSchema):
     assignee_email: EmailStr | None = Field(None, title="Почта исполнителя",
                              description="Если обновлять поле не надо - оставить None",
                              examples=["metoks@gmail.com"])
+
+    @field_validator("title")
+    @classmethod
+    def validate_title(cls, value):
+        if value is not None and not (1 < len(value) <= 25):
+            raise ValueError("The title length must be between 1 and 25 characters")
+        return value
+
+    @field_validator("description")
+    @classmethod
+    def validate_description(cls, value):
+        if value is not None and not (1 < len(value) <= 200):
+            raise ValueError("The description length must be from 1 to 200 characters")
+        return value
